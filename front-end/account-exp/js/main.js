d94ba7adf9614a35defd746ffdabf650738243da
wangkang <wangkang@finogeeks.com>
2020-12-11 17:29:45 +0800

    feat:init

---
+++ front-end/account-exp/js/main.js
@@ -0,0 +1,32 @@
+let myHeading = document.querySelector('h1');
+console.log(myHeading.textContent);
+myHeading.textContent = 'Hello world!';
+
+let myImg = document.querySelector("img");
+myImg.onclick = function(){
+    let mySrc = myImg.getAttribute("src");
+    console.log(mySrc);
+    myImg.setAttribute('src','../image/dongman.jpg');
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '您好：' + storedName;
+}
+
+let myButton = document.querySelector("button");
+
+function setUserName() {
+    let myName = prompt('请输入你的名字: ');
+    
+    localStorage.setItem('name',myName);
+    myHeading.textContent = '您好：' + myName;
+}
+myButton.onclick = function() {
+    setUserName()
+}
+function Add(num1,num2) {
+    return num1 + num2;
+}
